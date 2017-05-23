b1f950895e7cdf4406671bfc61904be2fe0ac900
rukaroa <rukaroa@gmail.com>
2017-05-23 18:41:46 -0500

    server setup

---
+++ server.js
@@ -0,0 +1,44 @@
+//npm tools
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var request = require("request");
+var cheerio = require("cheerio");
+var exphbs = require("express-handlebars");
+
+//models
+
+//
+mongoose.Promise = Promise;
+
+var app = express();
+
+//handlebars
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.ser("view engine", "handlebars");
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("./public"));
+
+//mongoose bd config
+mongoose.connect("mongodb://localhost/news-scraper");
+var db = mongoose.connection;
+
+//show mongoose errors
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+});
+
+//routes
+require("./routes/api-routes.js")(app);
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
