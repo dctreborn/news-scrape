334190aa5957bca58c0585ee0a6012d1fe8164df
rukaroa <rukaroa@gmail.com>
2017-05-25 18:46:26 -0500

    routes started

---
+++ routes/api-routes.js
@@ -0,0 +1,78 @@
+module.exports = function(app) {
+    //scrape website
+    app.get("/scrape", function(res, res) {
+        request("http://reddit.com", function(error, resp, html) {
+            var $ = cheerio.load(html);
+
+            $("p .title").each(function(i, element) {
+                var result = {};
+
+                //change this after reading output
+                result.title = $(this).children("a").text();
+                result.link = $(this).children("a").attr("href");
+
+                var entry = new Article(result);
+
+                entry.save(function(err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(doc);
+                    }
+                });
+            });
+        });
+
+        res.send("Scape complete");
+    });
+
+    //get all articles
+    app.get("/articles", function(req, res) {
+        Article.find({}, function(err, doc) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json(doc);
+            }
+        });
+    });
+
+    //get article by ID
+    app.get("/articles/:id", function(req, res) {
+        Article.findOne({
+                _id: req.params.id
+            })
+            .populate("note")
+            .exec(function(err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json(doc);
+                }
+            })
+    });
+
+    //create new note or replacec existing note
+    app.post("/articles/:id", function(req, res) {
+        var newNote = new Note(req.body);
+
+        newNote.save(function(err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                Article.findOneAndUpdate({
+                        _id: req.params.id
+                    }, {
+                        note: doc._id
+                    })
+                    .exec(function(err, data) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.send(data);
+                        }
+                    });
+            }
+        });
+    });
+};
